01384d27435e7d73206dafc17c1a8e0ce646ec70
AntonioAlekseivich <antonbgi98@gmail.com>
2023-01-18 11:32:58 +0100

    Progreso en 04_07
    He agregado un if/else que comprueba que el valor esta entre 0 y 10 en
    la primera nota, falta agregarla en las otras 2

--- Aprende Java con Ejercicios/src/sentenciaCondiciona_If_Switch/Ejercicio7.java
+++ Aprende Java con Ejercicios/src/sentenciaCondiciona_If_Switch/Ejercicio7.java
@@ -3,18 +3,28 @@ import java.util.Scanner;
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		//Realiza un programa que calcule la media de tres notas.
+		//Realiza un programa que calcule la media de tres notas
 
 		//declaramos las variables
 Scanner s = new Scanner(System.in);
-int nota1;
-int nota2;
-int nota3;
+double nota1;
+double nota2;
+double nota3;
 
-int media;
-
+double media;
+while (true) {
+		//pedimos las notas
 System.out.println("¿Qué nota has sacado en el primer examen?");
 nota1 = s.nextInt();
+
+		//comprobamos que la nota introducida sea valida
+if(nota1>=0 && nota1<=10) {
+    break;
+}else {
+    System.out.println("Nota no valida, debe ser entre 0 y 10");
+    continue;
+	}
+}
 
 System.out.println("¿En el segundo?");
 nota2 = s.nextInt();
@@ -22,8 +32,10 @@ nota2 = s.nextInt();
 System.out.println("¿Y en el tercero?");
 nota3 = s.nextInt();
 
+		//calculamos la media
 media = (nota1+nota2+nota3)/3;
-System.out.println("La media de las 3 notas es de "+media+".");
+System.out.printf("La media de las 3 notas es de %.2f.\n",media);
+
 	}
 
 }
