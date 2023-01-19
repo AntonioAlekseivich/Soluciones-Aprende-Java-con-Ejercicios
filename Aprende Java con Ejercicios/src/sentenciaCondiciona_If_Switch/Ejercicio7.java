a969d4cb353aa01f389efa5eca7f94c2536c2413
AntonioAlekseivich <antonbgi98@gmail.com>
2023-01-19 10:58:37 +0100

    terminado 04_07
    He agregado un while a cada nota para comprobar que el valor introducido
    esta entre 0 y 10

--- Aprende Java con Ejercicios/src/sentenciaCondiciona_If_Switch/Ejercicio7.java
+++ Aprende Java con Ejercicios/src/sentenciaCondiciona_If_Switch/Ejercicio7.java
@@ -12,7 +12,10 @@ double nota2;
 double nota3;
 
 double media;
+
+		//while para comprobar que el input de la nota es un numero valido
 while (true) {
+	
 		//pedimos las notas
 System.out.println("¿Qué nota has sacado en el primer examen?");
 nota1 = s.nextInt();
@@ -26,11 +29,34 @@ if(nota1>=0 && nota1<=10) {
 	}
 }
 
+		//segundo while
+while (true) {
+	
+		//pedimos la segunda nota
 System.out.println("¿En el segundo?");
 nota2 = s.nextInt();
 
+if(nota2>=0 && nota2<=10) {
+    break;
+}else {
+    System.out.println("Nota no valida, debe ser entre 0 y 10");
+    continue;
+	}
+}
+
+		//tercer while
+while (true) {
 System.out.println("¿Y en el tercero?");
 nota3 = s.nextInt();
+	
+		//pedimos la tercera nota
+if(nota3>=0 && nota3<=10) {
+    break;
+}else {
+    System.out.println("Nota no valida, debe ser entre 0 y 10");
+    continue;
+	}
+}
 
 		//calculamos la media
 media = (nota1+nota2+nota3)/3;
