69e7a586ff141b6660721d3ca4361d9e3ce128df
AntonioAlekseivich <antonbgi98@gmail.com>
2023-01-22 23:34:17 +0100

    2_06 terminado
    Me daba error por estar reasignando los string dentro del bucle, la
    solucion es concatenarlos.

--- Aprende Java con Ejercicios/Ejercicios/unidad_06_NumerosAleatorios/Ejercicio02_6.java
+++ Aprende Java con Ejercicios/Ejercicios/unidad_06_NumerosAleatorios/Ejercicio02_6.java
@@ -11,66 +11,73 @@ public class Ejercicio02_6 {
 		String.valueOf(n)*/
 
 //variables:
-		String palo;//puede ser ("pica","corazones","diamantes","treboles")(4casos)
-		String carta;//puede ser 4 casos o un numero del 2 al 10
+		String palo = "";//puede ser ("pica","corazones","diamantes","treboles")(4casos)
+		String carta = "";//puede ser 4 casos o un numero del 2 al 10
 		int rollPalo;
 		int rollCarta;
 		
 			
-//sacamos el valor del palo
-		rollPalo = (int)(Math.random()*4 ) + 1 ;
-			switch (rollPalo) {
-			case 1:
-				palo = "Picas";
-				System.out.println(palo);
-			break;
-			case 2: 
-				palo = "Corazones";
-				System.out.println(palo);
-			break;
-			case 3:
-				palo = "Diamantes";
-				System.out.println(palo);
-			break;
-			case 4:
-				palo = "Tréboles";
-				System.out.println(palo);
-			break;
-			
-			}
+
 			
 			
-//sacamos el valor de la carta
+			//sacamos el valor de la carta
 		rollCarta = (int)(Math.random()* 13 ) + 1;
 		
 			//separamos los casos que pueden ser caracteres de los numerales
 			if (rollCarta == 1) {
-						carta = "A";
-						System.out.println(carta);
+						carta += "A";
+						
 			} else if (rollCarta > 10){
 				
 				switch(rollCarta) {
 					case 11:
-						carta = "J";
-						System.out.println(carta);
+						carta += "J";
+						
 						break;
 					case 12:
-						carta = "Q";
-						System.out.println(carta);
+						carta += "Q";
+						
 						break;
 					case 13:
-					carta = "K";
-					System.out.println(carta);
+					carta += "K";
+					
 					break;
 				}
 				
 			}else {
-			carta = String.valueOf(rollCarta);
-			System.out.println(carta);
+			carta += rollCarta;
+			
 			
 			}
 			
 			
+			
+			//sacamos el valor del palo
+			rollPalo = (int)(Math.random()*4 ) + 1 ;
+				switch (rollPalo) {
+				case 1:
+					palo += "Picas";
+					
+					
+				break;
+				case 2: 
+					palo += "Corazones";
+					
+					
+				break;
+				case 3:
+					palo += "Diamantes";
+					
+				break;
+				case 4:
+					palo += "Tréboles";
+					
+				break;
+				
+				}
+			
+			
+	System.out.println(carta+" de "+palo);
 	}	
 }
 
