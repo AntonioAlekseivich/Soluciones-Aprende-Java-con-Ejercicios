deb7fbf2ffced0c8c855fa737972c1c077f5b4bd
AntonioAlekseivich <antonbgi98@gmail.com>
2023-01-24 18:31:32 +0100

    Creado 07_6, todavia no hay nada de codigo

---
+++ Aprende Java con Ejercicios/Ejercicios/unidad_06_NumerosAleatorios/Ejercicio07_6.java
@@ -0,0 +1,11 @@
+package unidad_06_NumerosAleatorios;
+
+public class Ejercicio07_6 {
+
+	public static void main(String[] args) {
+		// Escribe un programa que muestre tres apuestas de la quiniela en tres columnas
+		//para los 14 partidos y el pleno al quince (15 filas)
+
+	}
+
+}
