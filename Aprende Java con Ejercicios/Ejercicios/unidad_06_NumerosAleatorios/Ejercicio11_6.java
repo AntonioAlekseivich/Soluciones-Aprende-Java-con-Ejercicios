205c7cc081a1dbd33cb3d05f36d4404b85b6850d
anton <anton@ASUS1312>
2023-01-31 12:07:09 +0100

    empezado 11_6

---
+++ Aprende Java con Ejercicios/Ejercicios/unidad_06_NumerosAleatorios/Ejercicio11_6.java
@@ -0,0 +1,36 @@
+package unidad_06_NumerosAleatorios;
+import java.util.Scanner;
+
+public class Ejercicio11_6 {
+
+	public static void main(String[] args) {
+		/* Realiza un programa que pida 10 números por teclado y que los almacene en
+		un array. A continuación se mostrará el contenido de ese array junto al índice
+		(0 – 9) utilizando para ello una tabla. Seguidamente el programa pasará los
+		primos a las primeras posiciones, desplazando el resto de números (los que no
+		son primos) de tal forma que no se pierda ninguno. Al final se debe mostrar el
+		array resultante.*/
+		Scanner s = new Scanner(System.in);
+		int [] numeros = new int [10];
+	
+		for (int i = 0; i<10; i++) {
+			System.out.println("Introduzca un numero");
+			numeros[i] = s.nextInt();
+			
+		}
+		for ( int i = 0; i<10; i++) {
+			System.out.print(" "+i+" ");
+			
+		}
+		System.out.println();//salto de linea
+		for ( int i = 0; i<10; i++) {
+			System.out.print(" "+numeros[i]+" ");
+			
+		}
+		for ( int i = 0; i<10; i++) {
+			while(numeros[i]%2==0){
+		}
+	}
+
+}
+}
