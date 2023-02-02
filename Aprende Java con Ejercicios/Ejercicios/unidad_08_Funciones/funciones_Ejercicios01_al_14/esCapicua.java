9fe847e82b7d1f408bed0078364740d7085f436e
AntonioAlekseivich <antonbgi98@gmail.com>
2023-02-02 20:39:50 +0100

    para subir la reoorganizacion del paquete de las funciones de tema 8

--- Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/funciones_Ejercicios01_al_14/esCapicua.java
+++ Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/funciones_Ejercicios01_al_14/esCapicua.java
@@ -1,5 +1,5 @@
 package unidad_08_Funciones.funciones_Ejercicios01_al_14;
 
 public class esCapicua {
-//para subir al stage area
+//para subir el merge que reorganiza las funciones en el paquete de forma correcta
 }
