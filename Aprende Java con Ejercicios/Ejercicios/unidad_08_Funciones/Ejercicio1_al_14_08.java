545d6291d610d82f245f2aba71e7b5e59b8936ab
AntonioAlekseivich <antonbgi98@gmail.com>
2023-02-04 18:18:23 +0100

    Reoorganizado el ejercicio de las funciones y escrito un poco de codigo

---
+++ Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/Ejercicio1_al_14_08.java
@@ -0,0 +1,29 @@
+package unidad_08_Funciones;
+
+public class Ejercicio1_al_14_08 {
+	
+	/*Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario.
+	 * 
+	 * @param x un número entero positivo
+	 * @return <code>true</code> si el número es primo
+	 * @return <code>false</code> en caso contrario
+	 * 
+	 * */
+	
+	public static boolean esCapicua(int x) {
+		int cifras [] = new int [10];
+		int cifrasInv [] = new int [10];
+		
+		
+		for (int i = 0; i < 10; i++) {
+			
+			if(cifras == cifrasInv) {
+				return true;
+			} else {
+				return false;
+			}
+			
+		}
+		
+	}
+}
--- Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/funciones_Ejercicios01_al_14/esCapicua.java
+++
@@ -1,5 +0,0 @@
-package unidad_08_Funciones.funciones_Ejercicios01_al_14;
-
-public class esCapicua {
-//para subir el merge que reorganiza las funciones en el paquete de forma correcta
-}
