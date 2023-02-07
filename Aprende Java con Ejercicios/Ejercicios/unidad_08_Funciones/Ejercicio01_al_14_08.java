6de68655782d0b8b56260513e9a238cdac7fb84d
AntonioAlekseivich <antonbgi98@gmail.com>
2023-02-07 13:30:19 +0100

    Hecho ejercicio del 01_14 al 10_14.
    He empezado un metodo para parsear de array a string, pero igual no hace
    falta teniendo las librerias de java.

--- Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/Ejercicio01_al_14_08.java
+++ Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/Ejercicio01_al_14_08.java
@@ -1,55 +1,66 @@
 package unidad_08_Funciones;
 
-import java.util.Arrays;
 
 public class Ejercicio01_al_14_08 {
-	
-	/*Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario.
-	 * 
-	 * @param x un número entero positivo
-	 * @return <code>true</code> si el número es primo
-	 * @return <code>false</code> en caso contrario
-	 * 
-	 * */
-	
-	public static boolean esCapicua(int x) {
-	
-		int nDigitos = 0;
-		int u = x;//calculamos cuantos digitos tiene x
-		while(u>0) {
-			u = u/10;
-			nDigitos++;
-		}
 
-		int [] digitos = new int [nDigitos];//declaramos los 2 arrays que vamos a usar
-		int [] digitosInv = new int [nDigitos];
-		
-		for (int i=0; i < nDigitos; i++) {//atribuimos los digitos de x al array digitos
+	/*Metodo para pasar los digitos de un int a un array, se usa mucho.
+	 * 
+	 * @param int x 
+	 * return digitos
+	 */
+	public static int[] getArray (int x) {
+
+		int nDigitos = (Ejercicio01_al_14_08.digitos(x));//contamos los digitos
+
+		int [] digitos = new int [nDigitos];//declaramos array
+
+		for (int i=(nDigitos - 1); i >= 0; i--) {//atribuimos los digitos de x al array 
 			digitos [i] = x%10;
 			x = x/10;
 		}
-		
-		int k = 0;
-		for (int i = (nDigitos - 1); i>=0; i--) {//creamos el array invertido
-		digitosInv[k] = digitos[i];
-		k++;
+		return digitos;
+	}
+
+	/*Concatenamos un array a un String
+	 * 
+	 * @param int arr [] array a concatenar
+	 * 
+	 * @return String igual al array
+	 */
+	public static String arrayToString (int x[]) {
+		String resultado = "";
+		for(int i = 0; i<x.length; i++) {
+			 resultado += x [i];
 		}
-		
-		if (Arrays.equals(digitos, digitosInv)) {//comparamos los arrays
-			return true;
-		} else {
-			return false;
-		}
+		return resultado;
 	}
 	
-	/*: Devuelve verdadero si el número que se pasa como parámetro es primo y falso en caso contrario.
+	
+	/* 01_Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario.
 	 * 
 	 * @param x un número entero positivo
 	 * @return <code>true</code> si el número es primo
 	 * @return <code>false</code> en caso contrario
 	 * 
 	 * */
-	
+	public static boolean esCapicua(int x) {
+
+		int u = (Ejercicio01_al_14_08.voltea(x));
+
+		if (x == u) {//comparamos los ints
+			return true;
+		} else { 
+			return false;
+		}
+	}
+
+	/* 02_Devuelve verdadero si el número que se pasa como parámetro es primo y falso en caso contrario.
+	 * 
+	 * @param x un número entero positivo
+	 * @return <code>true</code> si el número es primo
+	 * @return <code>false</code> en caso contrario
+	 * 
+	 * */
 	public static boolean esPrimo(int x) {
 
 		for (int i=1; i<=x; i++) {//recorremos todos los num posibles
@@ -59,5 +70,179 @@ public class Ejercicio01_al_14_08 {
 		}
 		return true;//si no se entro en el if, es true
 	}
+
+
+	/* 03_Devuelve el menor primo que es mayor al número que se pasa como parámetro.
+	 * 
+	 * @param x un número entero positivo
+	 * @return int primo siguiente al param
+	 * 
+	 * */
+	public static int siguientePrimo (int x) {
+		int nPrimo = 0;//iniciamos el primo
+
+		while(nPrimo == 0) {
+			x++;//incrementamos el param
+			if(Ejercicio01_al_14_08.esPrimo(x)) {//si es primo lo atribuimos
+				nPrimo = x;
+			}
+		}
+		return nPrimo;
+	}
+
+
+	/* 04_Dada una base y un exponente devuelve la potencia.
+	 * 
+	 * @param x un número entero positivo de base
+	 * @param x un número entero positivo de exponente
+	 * 
+	 * @return int potencia de resultado
+	 * 
+	 * */
+	public static int potencia (int x, int y) {
+
+		int potencia = (int) Math.pow(x, y);
+		return potencia;
+	}
+
+
+	/* 05_Cuenta el número de dígitos de un número entero.
+	 * 
+	 * @param x un número entero positivo 
+	 * 
+	 * @return int nº digitos de x
+	 * 
+	 * */
+	public static int digitos (int x) {
+
+		int nDigitos = 0;
+
+		while(x>0) {//mientras se puede, dividimos x entre 10
+			x = x/10;
+			nDigitos++;//x cada iteracion, sumamos 1
+		}
+		return nDigitos;
+	}
+
+
+	/* 06_Le da la vuelta a un número
+	 * 
+	 * @param x un número entero positivo 
+	 * 
+	 * @return int xVolteado
+	 * 
+	 * */
+	public static int voltea (int x) {
+
+		int nDigitos = (Ejercicio01_al_14_08.digitos(x));//calculamos el nº digitos
+		int [] digitos = (Ejercicio01_al_14_08.getArray(x));//declaramos array con los digitos
+
+
+		String resultado = "";//concatenamos en string
+		for(int i = (nDigitos - 1); i >= 0; i--) {//en orden inverso
+			resultado += digitos[i];
+		}
+
+		return Integer.parseInt(resultado);//pasamos a int
+	}
+
+
+	/* 07_Devuelve el dígito que está en la posición n de un número entero. Se empieza contando por el 0 y de izquierda a derecha.
+	 * 
+	 * @param int x un número entero positivo 
+	 * @param int n la posicion que vamos a comprobar
+	 * 
+	 * @return int k el numero en la posicion
+	 * 
+	 * */
+	public static int digitoN (int x, int n) {
+
+		int [] digitos = (Ejercicio01_al_14_08.getArray(x));//declaramos array con los digitos
+
+		int k = digitos [n];
+		return k;
+	}
+
+
+	/* 08_Da la posición de la primera ocurrencia de un dígito
+	 *	dentro de un número entero. Si no se encuentra, devuelve -1.
+	 * 
+	 * @param int x un número entero positivo 
+	 * @param int k el num que vamos a buscar
+	 * 
+	 * @return la posicion empezando por 0 y de izquierda a derecha.
+	 * 
+	 * */
+	public static int posicionDeDigito (int x, int k) {
+
+		int nDigitos = (Ejercicio01_al_14_08.digitos(x));//contamos los digitos
+		int [] digitos = new int [nDigitos];//declaramos array
+
+		for (int i=(nDigitos - 1); i >= 0; i--) {//atribuimos los digitos de x al array 
+			digitos [i] = x%10;
+			x = x/10;
+		}
+		for (int i=0; i<nDigitos; i++) {
+			if ( digitos[i] == k) {
+				return i;
+			}
+		} return -1;
+	}
+
+
+	/* 09_Le quita a un número n dígitos por detrás (por la derecha).
+	 * 
+	 * @param int x un número entero positivo 
+	 * @param int n la cantidad que vamos a quitar
+	 * 
+	 * @return int con los digitos quitados
+	 * 
+	 * */
+	public static int quitaPorDetras (int x, int n) {
+		int [] digitos = (Ejercicio01_al_14_08.getArray(x));//Array de los digitos
+		String resultado = "";//para poder concatenar el resultado
+
+		for(int i = 0; i<(digitos.length - n); i++) {
+			resultado += digitos [i];
+		}
+		return Integer.parseInt(resultado);
+	}
+
+	/* 10_Le quita a un número n dígitos por delante (por la izquierda).
+	 * 
+	 * @param int x un número entero positivo 
+	 * @param int n la cantidad que vamos a quitar
+	 * 
+	 * @return int con los digitos quitados
+	 * 
+	 * */	
+	public static int quitaPorDelante (int x, int n) {
+		int [] digitos = (Ejercicio01_al_14_08.getArray(x));//Array de los digitos
+		String resultado = "";//para poder concatenar el resultado
+
+		for(int i = n; i<digitos.length; i++) {
+			resultado += digitos [i];
+		}
+		return Integer.parseInt(resultado);
+	}
+
+	/* 11_Añade un dígito a un número por detrás.
+	 * 
+	 * @param int x un número entero positivo 
+	 * @param int n digito que se va a pegar
+	 * 
+	 * @return int con x concatenado 
+	 * 
+	 * */	
+	/*public static int pegaPorDetras (int x, int n) {
+		int [] digitos = (Ejercicio01_al_14_08.getArray(x));//Array de los digitos
+		String resultado = "";//para poder concatenar el resultado
+		
+		for(int i = 0; i<digitos.length; i++) {
+			resultado += digitos [i];
+		}
+		return Integer.parseInt(resultado);
+	}*/
 }
+
 
--- Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/TestFunciones.java
+++ Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/TestFunciones.java
@@ -7,13 +7,63 @@ import org.junit.jupiter.api.Test;
 class TestFunciones {
 
 	@Test
+	void getArray() {//codigo escrito por chatGPT
+	    int[] actual = Ejercicio01_al_14_08.getArray(1436);
+	    int[] expected = {1, 4, 3, 6};
+	    assertArrayEquals(expected, actual);}
+	
+	@Test
 	void esCapicua() {
-		assertEquals(Ejercicio01_al_14_08.esCapicua(1331),true);
+		assertEquals(Ejercicio01_al_14_08.esCapicua(53335),true);
+		assertEquals(Ejercicio01_al_14_08.esCapicua(5333),false);
 	}
 	
 	@Test
 	void esPrimo() {
 		assertEquals(Ejercicio01_al_14_08.esPrimo(7),true);
+		assertEquals(Ejercicio01_al_14_08.esPrimo(8),false);
+	}
+	
+	@Test
+	void siguientePrimo() {
+		assertEquals(Ejercicio01_al_14_08.siguientePrimo(11),13);
+	}
+	
+	@Test
+	void potencia() {
+		assertEquals(Ejercicio01_al_14_08.potencia(8,6), 262144);
 	}
 
+	@Test
+	void digitos() {
+		assertEquals(Ejercicio01_al_14_08.digitos(6439), 4);
+	}
+	
+	@Test
+	void voltea() {
+		assertEquals(Ejercicio01_al_14_08.voltea(64398), 89346);
+	}
+	
+	@Test
+	void digitoN() {
+		assertEquals(Ejercicio01_al_14_08.digitoN(64398, 0), 6);
+	}
+
+	@Test
+	void posicionDeDigitoEncontrado() {
+		assertEquals(Ejercicio01_al_14_08.posicionDeDigito(64398, 9), 3);
+		assertEquals(Ejercicio01_al_14_08.posicionDeDigito(64398, 7), -1);
+	}
+	
+	@Test
+	void quitaPorDetras() {
+		assertEquals(Ejercicio01_al_14_08.quitaPorDetras(6439823, 1), 643982);
+	}
+	
+	@Test
+	void quitaPorDelante() {
+		assertEquals(Ejercicio01_al_14_08.quitaPorDelante(6439823, 1), 439823);
+	}
+	
+	
 }
