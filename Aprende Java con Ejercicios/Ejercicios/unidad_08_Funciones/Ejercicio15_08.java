f9588803716d25ad61034ec15d27d2037b327fdf
AntonioAlekseivich <antonbgi98@gmail.com>
2023-02-07 16:54:56 +0100

    Terminado 15_08

--- Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/Ejercicio15_08.java
+++ Aprende Java con Ejercicios/Ejercicios/unidad_08_Funciones/Ejercicio15_08.java
@@ -1,5 +1,15 @@
 package unidad_08_Funciones;
 
 public class Ejercicio15_08 {
-//Para subir al stage area
+	public static void main(String[] args) {
+		//Muestra los números primos que hay entre 1 y 1000.
+		for (int i = 1; i <= 1000; i++) {//loop del 1 al 1000
+			if(Ejercicio01_al_14_08.esPrimo(i)) {
+				System.out.println(i);
+			}
+		}
+
+	}
 }
+
+
