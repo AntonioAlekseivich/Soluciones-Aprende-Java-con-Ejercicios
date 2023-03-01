73d47d3d5ae97d0c1925d0bf1a64c0b785462561
AntonioAlekseivich <antonbgi98@gmail.com>
2023-03-01 12:44:28 +0100

    Ejercicio 4 hecho, falta entenderlo del todo y comentarlo

---
+++ Aprende Java con Ejercicios/Ejercicios/unidad_09_ProgramacionOrientadaAObjetos/Ejercicio04/FraccionTest.java
@@ -0,0 +1,27 @@
+package unidad_09_ProgramacionOrientadaAObjetos.Ejercicio04;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class FraccionTest {
+
+	@Test
+	void invierteTest() {
+		int [] expected = {3,4};
+		int [] result = Fraccion.invierte(4,3);
+        assertArrayEquals(expected, result);
+	}
+	@Test
+	void simplificaTest() {
+		int [] expected = {1,2};
+		int [] result = Fraccion.simplifica(8,16);
+        assertArrayEquals(expected, result);
+	}
+	@Test
+	void simplificaTest2() {
+		int [] expected = {1,3};
+		int [] result = Fraccion.simplifica(3,9);
+        assertArrayEquals(expected, result);
+	}
+
+}
