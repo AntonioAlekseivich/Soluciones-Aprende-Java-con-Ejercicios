41681c8b007f0ab9c046e09987e3ea4110200f76
anton <antonbgi98@gmail.com>
2023-10-04 17:14:46 +0200

    Actualizados 3 ejercicios

--- Aprende Java con Ejercicios/Ejercicios/unidad_01_HolaMundoSalidaDeDatos/Ejercicio07_1.java
+++ Aprende Java con Ejercicios/Ejercicios/unidad_01_HolaMundoSalidaDeDatos/Ejercicio07_1.java
@@ -3,7 +3,9 @@ package unidad_01_HolaMundoSalidaDeDatos;
 public class Ejercicio07_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Igual que el programa anterior, pero esta vez la pirámide estará hueca,
+		*(se debe ver únicamente el contorno(*/
+		
 		System.out.println("    *    ");
 		System.out.println("   * *   ");
 		System.out.println("  *   *  ");
--- Aprende Java con Ejercicios/Ejercicios/unidad_09_ProgramacionOrientadaAObjetos/Ejercicio05/Pizza.java
+++ Aprende Java con Ejercicios/Ejercicios/unidad_09_ProgramacionOrientadaAObjetos/Ejercicio05/Pizza.java
@@ -6,7 +6,7 @@ quesos o funghi - y su estado - pedida o servida. La clase debe almacenar inform
 han servido. Siempre que se crea una pizza nueva, su estado es “pedida”. 
 El siguiente código del programa principal debe dar la salida que se muestra:Crea la clase Pizza con los atributos y métodos necesarios. 
 Sobre cada pizza se necesita saber el tamaño - mediana o familiar - el tipo - margarita, cuatro quesos o funghi - y su estado - pedida o servida.
- La clase debe almacenarinformación sobre el número total de pizzas que se han pedido y que se han servido. 
+ La clase debe almacenar información sobre el número total de pizzas que se han pedido y que se han servido. 
  Siempre que se crea una pizza nueva, su estado es “pedida”. 
  El siguiente código del programa principal debe dar la salida que se muestra:*/
 	
